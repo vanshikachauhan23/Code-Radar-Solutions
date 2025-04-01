fb8ad833127f8dd58fce7432353efc7b3ff456e7
Vanshika Chauhan <chauhan23vanshika@gmail.com>
2025-04-01 13:24:15 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,16 @@
+void fabonacciSeries(int n)
+{
+    for(i=0;i<n;i++)
+    {
+        printf(" %d",fab(i));
+    }
+}
+int fab(int n)
+{
+    if(n==0)
+    return 0;
+    if(n==1)
+    return 1;
+    return fab(n-1) + fab(n-2);
+
+}
